675da97199f27b1ebf2ff056d71aa8345aa695c0
ymeister <47071325+ymeister@users.noreply.github.com>
2026-03-17 02:16:28 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-          project = pkgs.callPackage ./default.nix { inherit pkgs; };
-      in {
-        packages = project;
-      }
-    );
+  outputs = { self, nixpkgs, ... }:
+    let eachSystem = nixpkgs.lib.genAttrs
+          [ "x86_64-linux"
+            "aarch64-linux"
+          ];
+    in {
+      packages = eachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+            project = pkgs.callPackage ./default.nix { inherit pkgs; };
+        in {
+          default = project;
+        }
+      );
+    };
 }
